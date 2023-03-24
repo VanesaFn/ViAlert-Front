3dae3bebeb0c8eabaa048c84d50f13d3e2111b67
Hanna Obando <hannaobandopk@gmail.com>
2023-03-24 13:23:24 -0500

    cambio en los colores

---
+++ app/src/main/java/com/example/vialert_front/ConfirmacionCorreo.kt
@@ -0,0 +1,11 @@
+package com.example.vialert_front
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConfirmacionCorreo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmacion_correo)
+    }
+}
