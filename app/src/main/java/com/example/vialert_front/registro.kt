37d281bc5f2a2900208b1fdb912f950b9d6d8e2e
VanesaFn <vanefndzt@gmail.com>
2023-03-24 16:23:35 -0500

    cambios, primeras dos vistas

--- app/src/main/java/com/example/vialert_front/registro.kt
+++ app/src/main/java/com/example/vialert_front/registro.kt
@@ -3,9 +3,9 @@ package com.example.vialert_front
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class registro : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_inicio_sesion)
+        setContentView(R.layout.activity_registro)
     }
 }
