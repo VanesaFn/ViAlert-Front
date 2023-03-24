d8b16ff12fe51407bac0e0e09cca942ad9d87f02
jms2711 <jhntnmnzn@gmail.com>
2023-03-24 16:12:11 -0500

    generacion dos ultimas vistas

---
+++ app/src/main/java/com/example/vialert_front/lista_usuarios.kt
@@ -0,0 +1,11 @@
+package com.example.vialert_front
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class lista_usuarios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_usuarios)
+    }
+}
