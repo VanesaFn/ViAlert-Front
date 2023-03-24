9fd790283c2ab7acebff6ce5c147021994c27f0a
Hanna Obando <hannaobandopk@gmail.com>
2023-03-24 15:01:26 -0500

    generacion de codigo random y pantalla de confirmacion terminada

--- app/src/main/java/com/example/vialert_front/ConfirmacionCorreo.kt
+++ app/src/main/java/com/example/vialert_front/ConfirmacionCorreo.kt
@@ -1,11 +1,24 @@
 package com.example.vialert_front
 
 import androidx.appcompat.app.AppCompatActivity
+import com.example.vialert_front.databinding.ActivityConfirmacionCorreoBinding
 import android.os.Bundle
+import kotlin.random.Random
 
 class ConfirmacionCorreo : AppCompatActivity() {
+    lateinit var binding: ActivityConfirmacionCorreoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmacion_correo)
+
+        val codigo = codigo().toString()
+        binding.TxtConfirmacion.setText(codigo)
+
+    }
+
+    private fun codigo():Int{
+        var randomNumber:Int
+        randomNumber= Random.nextInt(100000,999999)
+        return randomNumber
     }
 }
